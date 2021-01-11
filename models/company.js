68e8c54f70b16c49fe9f2e36f5c84024bc517b0e
Steven Tejeda <steventejeda@gmail.com>
2021-01-11 02:35:14 -0500

    final

--- models/company.js
+++ models/company.js
@@ -7,108 +7,151 @@ const { sqlForPartialUpdate } = require("../helpers/sql");
 /** Related functions for companies. */
 
 class Company {
-  /** Create a company (from data), update db, return new company data.
-   *
-   * data should be { handle, name, description, numEmployees, logoUrl }
-   *
-   * Returns { handle, name, description, numEmployees, logoUrl }
-   *
-   * Throws BadRequestError if company already in database.
-   * */
+    /** Create a company (from data), update db, return new company data.
+     *
+     * data should be { handle, name, description, numEmployees, logoUrl }
+     *
+     * Returns { handle, name, description, numEmployees, logoUrl }
+     *
+     * Throws BadRequestError if company already in database.
+     * */
 
-  static async create({ handle, name, description, numEmployees, logoUrl }) {
-    const duplicateCheck = await db.query(
-          `SELECT handle
+    static async create({ handle, name, description, numEmployees, logoUrl }) {
+        const duplicateCheck = await db.query(
+            `SELECT handle
            FROM companies
-           WHERE handle = $1`,
-        [handle]);
+           WHERE handle = $1`, [handle]);
 
-    if (duplicateCheck.rows[0])
-      throw new BadRequestError(`Duplicate company: ${handle}`);
+        if (duplicateCheck.rows[0])
+            throw new BadRequestError(`Duplicate company: ${handle}`);
 
-    const result = await db.query(
-          `INSERT INTO companies
+        const result = await db.query(
+            `INSERT INTO companies
            (handle, name, description, num_employees, logo_url)
            VALUES ($1, $2, $3, $4, $5)
-           RETURNING handle, name, description, num_employees AS "numEmployees", logo_url AS "logoUrl"`,
-        [
-          handle,
-          name,
-          description,
-          numEmployees,
-          logoUrl,
-        ],
-    );
-    const company = result.rows[0];
+           RETURNING handle, name, description, num_employees AS "numEmployees", logo_url AS "logoUrl"`, [
+                handle,
+                name,
+                description,
+                numEmployees,
+                logoUrl,
+            ],
+        );
+        const company = result.rows[0];
 
-    return company;
-  }
+        return company;
+    }
 
-  /** Find all companies.
-   *
-   * Returns [{ handle, name, description, numEmployees, logoUrl }, ...]
-   * */
+    /** Find all companies (optional filter on searchFilters).
+     *
+     * searchFilters (all optional):
+     * - minEmployees
+     * - maxEmployees
+     * - name (will find case-insensitive, partial matches)
+     *
+     * Returns [{ handle, name, description, numEmployees, logoUrl }, ...]
+     * */
 
-  static async findAll() {
-    const companiesRes = await db.query(
-          `SELECT handle,
+    static async findAll(searchFilters = {}) {
+        let query = `SELECT handle,
+                        name,
+                        description,
+                        num_employees AS "numEmployees",
+                        logo_url AS "logoUrl"
+                 FROM companies`;
+        let whereExpressions = [];
+        let queryValues = [];
+
+        const { minEmployees, maxEmployees, name } = searchFilters;
+
+        if (minEmployees > maxEmployees) {
+            throw new BadRequestError("Min employees cannot be greater than max");
+        }
+
+        // For each possible search term, add to whereExpressions and queryValues so
+        // we can generate the right SQL
+
+        if (minEmployees !== undefined) {
+            queryValues.push(minEmployees);
+            whereExpressions.push(`num_employees >= $${queryValues.length}`);
+        }
+
+        if (maxEmployees !== undefined) {
+            queryValues.push(maxEmployees);
+            whereExpressions.push(`num_employees <= $${queryValues.length}`);
+        }
+
+        if (name) {
+            queryValues.push(`%${name}%`);
+            whereExpressions.push(`name ILIKE $${queryValues.length}`);
+        }
+
+        if (whereExpressions.length > 0) {
+            query += " WHERE " + whereExpressions.join(" AND ");
+        }
+
+        // Finalize query and return results
+
+        query += " ORDER BY name";
+        const companiesRes = await db.query(query, queryValues);
+        return companiesRes.rows;
+    }
+
+    /** Given a company handle, return data about company.
+     *
+     * Returns { handle, name, description, numEmployees, logoUrl, jobs }
+     *   where jobs is [{ id, title, salary, equity }, ...]
+     *
+     * Throws NotFoundError if not found.
+     **/
+
+    static async get(handle) {
+        const companyRes = await db.query(
+            `SELECT handle,
                   name,
                   description,
                   num_employees AS "numEmployees",
                   logo_url AS "logoUrl"
            FROM companies
-           ORDER BY name`);
-    return companiesRes.rows;
-  }
+           WHERE handle = $1`, [handle]);
 
-  /** Given a company handle, return data about company.
-   *
-   * Returns { handle, name, description, numEmployees, logoUrl, jobs }
-   *   where jobs is [{ id, title, salary, equity, companyHandle }, ...]
-   *
-   * Throws NotFoundError if not found.
-   **/
+        const company = companyRes.rows[0];
 
-  static async get(handle) {
-    const companyRes = await db.query(
-          `SELECT handle,
-                  name,
-                  description,
-                  num_employees AS "numEmployees",
-                  logo_url AS "logoUrl"
-           FROM companies
-           WHERE handle = $1`,
-        [handle]);
+        if (!company) throw new NotFoundError(`No company: ${handle}`);
 
-    const company = companyRes.rows[0];
+        const jobsRes = await db.query(
+            `SELECT id, title, salary, equity
+           FROM jobs
+           WHERE company_handle = $1
+           ORDER BY id`, [handle],
+        );
 
-    if (!company) throw new NotFoundError(`No company: ${handle}`);
+        company.jobs = jobsRes.rows;
 
-    return company;
-  }
+        return company;
+    }
 
-  /** Update company data with `data`.
-   *
-   * This is a "partial update" --- it's fine if data doesn't contain all the
-   * fields; this only changes provided ones.
-   *
-   * Data can include: {name, description, numEmployees, logoUrl}
-   *
-   * Returns {handle, name, description, numEmployees, logoUrl}
-   *
-   * Throws NotFoundError if not found.
-   */
+    /** Update company data with `data`.
+     *
+     * This is a "partial update" --- it's fine if data doesn't contain all the
+     * fields; this only changes provided ones.
+     *
+     * Data can include: {name, description, numEmployees, logoUrl}
+     *
+     * Returns {handle, name, description, numEmployees, logoUrl}
+     *
+     * Throws NotFoundError if not found.
+     */
 
-  static async update(handle, data) {
-    const { setCols, values } = sqlForPartialUpdate(
-        data,
-        {
-          numEmployees: "num_employees",
-          logoUrl: "logo_url",
-        });
-    const handleVarIdx = "$" + (values.length + 1);
+    static async update(handle, data) {
+        const { setCols, values } = sqlForPartialUpdate(
+            data, {
+                numEmployees: "num_employees",
+                logoUrl: "logo_url",
+            });
+        const handleVarIdx = "$" + (values.length + 1);
 
-    const querySql = `UPDATE companies 
+        const querySql = `UPDATE companies 
                       SET ${setCols} 
                       WHERE handle = ${handleVarIdx} 
                       RETURNING handle, 
@@ -116,30 +159,29 @@ class Company {
                                 description, 
                                 num_employees AS "numEmployees", 
                                 logo_url AS "logoUrl"`;
-    const result = await db.query(querySql, [...values, handle]);
-    const company = result.rows[0];
+        const result = await db.query(querySql, [...values, handle]);
+        const company = result.rows[0];
 
-    if (!company) throw new NotFoundError(`No company: ${handle}`);
+        if (!company) throw new NotFoundError(`No company: ${handle}`);
 
-    return company;
-  }
+        return company;
+    }
 
-  /** Delete given company from database; returns undefined.
-   *
-   * Throws NotFoundError if company not found.
-   **/
+    /** Delete given company from database; returns undefined.
+     *
+     * Throws NotFoundError if company not found.
+     **/
 
-  static async remove(handle) {
-    const result = await db.query(
-          `DELETE
+    static async remove(handle) {
+        const result = await db.query(
+            `DELETE
            FROM companies
            WHERE handle = $1
-           RETURNING handle`,
-        [handle]);
-    const company = result.rows[0];
+           RETURNING handle`, [handle]);
+        const company = result.rows[0];
 
-    if (!company) throw new NotFoundError(`No company: ${handle}`);
-  }
+        if (!company) throw new NotFoundError(`No company: ${handle}`);
+    }
 }
 
 
--- routes/companies.js
+++ routes/companies.js
@@ -6,11 +6,12 @@ const jsonschema = require("jsonschema");
 const express = require("express");
 
 const { BadRequestError } = require("../expressError");
-const { ensureLoggedIn } = require("../middleware/auth");
+const { ensureAdmin } = require("../middleware/auth");
 const Company = require("../models/company");
 
 const companyNewSchema = require("../schemas/companyNew.json");
 const companyUpdateSchema = require("../schemas/companyUpdate.json");
+const companySearchSchema = require("../schemas/companySearch.json");
 
 const router = new express.Router();
 
@@ -21,22 +22,22 @@ const router = new express.Router();
  *
  * Returns { handle, name, description, numEmployees, logoUrl }
  *
- * Authorization required: login
+ * Authorization required: admin
  */
 
-router.post("/", ensureLoggedIn, async function (req, res, next) {
-  try {
-    const validator = jsonschema.validate(req.body, companyNewSchema);
-    if (!validator.valid) {
-      const errs = validator.errors.map(e => e.stack);
-      throw new BadRequestError(errs);
-    }
+router.post("/", ensureAdmin, async function(req, res, next) {
+    try {
+        const validator = jsonschema.validate(req.body, companyNewSchema);
+        if (!validator.valid) {
+            const errs = validator.errors.map(e => e.stack);
+            throw new BadRequestError(errs);
+        }
 
-    const company = await Company.create(req.body);
-    return res.status(201).json({ company });
-  } catch (err) {
-    return next(err);
-  }
+        const company = await Company.create(req.body);
+        return res.status(201).json({ company });
+    } catch (err) {
+        return next(err);
+    }
 });
 
 /** GET /  =>
@@ -50,13 +51,24 @@ router.post("/", ensureLoggedIn, async function (req, res, next) {
  * Authorization required: none
  */
 
-router.get("/", async function (req, res, next) {
-  try {
-    const companies = await Company.findAll();
-    return res.json({ companies });
-  } catch (err) {
-    return next(err);
-  }
+router.get("/", async function(req, res, next) {
+    const q = req.query;
+    // arrive as strings from querystring, but we want as ints
+    if (q.minEmployees !== undefined) q.minEmployees = +q.minEmployees;
+    if (q.maxEmployees !== undefined) q.maxEmployees = +q.maxEmployees;
+
+    try {
+        const validator = jsonschema.validate(q, companySearchSchema);
+        if (!validator.valid) {
+            const errs = validator.errors.map(e => e.stack);
+            throw new BadRequestError(errs);
+        }
+
+        const companies = await Company.findAll(q);
+        return res.json({ companies });
+    } catch (err) {
+        return next(err);
+    }
 });
 
 /** GET /[handle]  =>  { company }
@@ -67,13 +79,13 @@ router.get("/", async function (req, res, next) {
  * Authorization required: none
  */
 
-router.get("/:handle", async function (req, res, next) {
-  try {
-    const company = await Company.get(req.params.handle);
-    return res.json({ company });
-  } catch (err) {
-    return next(err);
-  }
+router.get("/:handle", async function(req, res, next) {
+    try {
+        const company = await Company.get(req.params.handle);
+        return res.json({ company });
+    } catch (err) {
+        return next(err);
+    }
 });
 
 /** PATCH /[handle] { fld1, fld2, ... } => { company }
@@ -84,36 +96,36 @@ router.get("/:handle", async function (req, res, next) {
  *
  * Returns { handle, name, description, numEmployees, logo_url }
  *
- * Authorization required: login
+ * Authorization required: admin
  */
 
-router.patch("/:handle", ensureLoggedIn, async function (req, res, next) {
-  try {
-    const validator = jsonschema.validate(req.body, companyUpdateSchema);
-    if (!validator.valid) {
-      const errs = validator.errors.map(e => e.stack);
-      throw new BadRequestError(errs);
-    }
+router.patch("/:handle", ensureAdmin, async function(req, res, next) {
+    try {
+        const validator = jsonschema.validate(req.body, companyUpdateSchema);
+        if (!validator.valid) {
+            const errs = validator.errors.map(e => e.stack);
+            throw new BadRequestError(errs);
+        }
 
-    const company = await Company.update(req.params.handle, req.body);
-    return res.json({ company });
-  } catch (err) {
-    return next(err);
-  }
+        const company = await Company.update(req.params.handle, req.body);
+        return res.json({ company });
+    } catch (err) {
+        return next(err);
+    }
 });
 
 /** DELETE /[handle]  =>  { deleted: handle }
  *
- * Authorization: login
+ * Authorization: admin
  */
 
-router.delete("/:handle", ensureLoggedIn, async function (req, res, next) {
-  try {
-    await Company.remove(req.params.handle);
-    return res.json({ deleted: req.params.handle });
-  } catch (err) {
-    return next(err);
-  }
+router.delete("/:handle", ensureAdmin, async function(req, res, next) {
+    try {
+        await Company.remove(req.params.handle);
+        return res.json({ deleted: req.params.handle });
+    } catch (err) {
+        return next(err);
+    }
 });
 
 
